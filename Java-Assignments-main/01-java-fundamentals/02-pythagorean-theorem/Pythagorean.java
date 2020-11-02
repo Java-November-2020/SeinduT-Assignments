b8f0d8ffbe79de106d525a72a7388d3488114863
SeinduT <you@example.com>
2020-11-01 17:11:54 -0700

    FizzBuzz

---
+++ Java-Assignments-main/01-java-fundamentals/02-pythagorean-theorem/Pythagorean.java
@@ -0,0 +1,5 @@
+public class Pythagorean {
+    public static double calculateHypothenuse (int lenA, int lenB) {
+        return Math.sqrt((lenA * lenA) + (lenB * lenB));
+    }
+}
---
+++ Java-Assignments-main/01-java-fundamentals/02-pythagorean-theorem/PythagoreanTest.java
@@ -0,0 +1,6 @@
+public class PythagoreanTest {
+    public static void main(int[] args) {
+        Pythagorean id = new Pythagorean();
+        id.calculateHypothenuse();
+    }
+}
